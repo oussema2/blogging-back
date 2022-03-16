5b5ac153ede16691d48c0a0c62bd83c653ffa98f
oussema2 <oussema.dabboussi99@gmail.com>
2022-03-16 23:26:44 +0100

    fix nodemon and test route

--- server.js
+++ server.js
@@ -3,13 +3,31 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const PORT = 4000;
-app.use(cors());
-app.use(express.json());
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
 
-//MONGODB CONNECTION
-/* mongoose.connect("mongodb://127.0.0.1:27017/freelancerDB", {
-  useNewUrlParser: true,
-}); */
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+app.use(express.json());
+app.use(cors());
 
 const connectionParams = {
   useNewUrlParser: true,
@@ -33,6 +51,9 @@ connection.once("open", function () {
   console.log("MongoDB database connection established successfully");
 });
 
+app.get("/index", function (req, res) {
+  res.send("running");
+});
 app.listen(PORT, function () {
   console.log("Server is running on Port: " + PORT);
 });
